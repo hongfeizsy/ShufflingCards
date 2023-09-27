a65d7c612a5b747db074e7804dbf6b11ac6f3f6c
hongfeizsy <hongfeizsy@gmail.com>
2023-09-27 23:23:37 +0200

    Add gender input field. But bug exists.

--- ShufflingCards/Assets/Game/Script/Input/CardSpawner.cs
+++ ShufflingCards/Assets/Game/Script/Input/CardSpawner.cs
@@ -2,28 +2,24 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
+using UnityEngine.UI;
 
 
 public class CardSpawner : MonoBehaviour
 {
     [SerializeField] TMP_InputField inputName;
     [SerializeField] TMP_InputField inputLevel;
+    [SerializeField] TMP_InputField inputGender;
+    [SerializeField] Button enterButton;
 
     public int selectionIndex = 0;
 
     private void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Tab) && Input.GetKey(KeyCode.LeftShift))
-        {
-            selectionIndex--;
-            if (selectionIndex < 0) selectionIndex = 1;
-            SelectInputField();
-        }
-
-        else if (Input.GetKeyDown(KeyCode.Tab))
+        if (Input.GetKeyDown(KeyCode.Tab))
         {
             selectionIndex++;
-            if (selectionIndex > 1) selectionIndex = 0;
+            if (selectionIndex > 3) selectionIndex = 0;
             SelectInputField();
         }
     }
@@ -38,6 +34,16 @@ public class CardSpawner : MonoBehaviour
             case 1:
                 inputLevel.Select();
                 break;
+            case 2:
+                print("Gender field selected.");
+                inputGender.Select();
+                break;
+            case 3:
+                enterButton.Select();
+                break;
+            // default:
+            //     enterButton.Select();
+            //     break;
         }
     }
 
@@ -45,14 +51,18 @@ public class CardSpawner : MonoBehaviour
 
     public void LevelSelected() { selectionIndex = 1; }
 
+    public void GenderSelected() { selectionIndex = 2; }
+
     public void SpawnCard() 
     {
-        Debug.Log(inputName.text + ": " + inputLevel.text);
+        Debug.Log(inputName.text + ", " + inputLevel.text + ", " + inputGender.text);
 
         inputName.Select();
         inputName.text = "";
         inputLevel.Select();
         inputLevel.text = "";
+        inputGender.Select();
+        inputGender.text = "";
 
         selectionIndex = 0;
     }
